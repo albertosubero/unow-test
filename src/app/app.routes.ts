51e44394aa33b19a3960312353d211eec1bc15ed
avsg1991@gmail.com <avsg1991@gmail.com>
2024-04-15 13:28:47 -0500

    UNOW-2: setup bootstrap and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EmployeesListComponent } from './core/modules/dashboard/employees-list/employees-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: EmployeesListComponent
+  }
+];
