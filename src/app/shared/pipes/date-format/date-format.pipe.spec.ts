69c3e75be766d72ff1976489143ff0f32eae5457
avsg1991@gmail.com <avsg1991@gmail.com>
2024-04-17 09:40:52 -0500

    UNOW-18: setup SearchFilterPipe and DateFormatPipe testing

--- src/app/shared/pipes/date-format/date-format.pipe.spec.ts
+++ src/app/shared/pipes/date-format/date-format.pipe.spec.ts
@@ -5,4 +5,14 @@ describe('DateFormatPipe', () => {
     const pipe = new DateFormatPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('Debe retornar fecha 1991/4/22', () => {
+    const pipe = new DateFormatPipe();
+    const date = {
+      day: 22,
+      month: 4,
+      year: 1991
+    }
+    expect(pipe.transform(date)).toEqual('1991/4/22');
+  });
 });
--- src/app/shared/pipes/search-filter/search-filter.pipe.spec.ts
+++ src/app/shared/pipes/search-filter/search-filter.pipe.spec.ts
@@ -1,8 +1,23 @@
+import { employeesListDummyData } from '../../dummyData/employees';
 import { SearchFilterPipe } from './search-filter.pipe';
 
 describe('SearchFilterPipe', () => {
   it('create an instance', () => {
     const pipe = new SearchFilterPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('Busqueda por nombre de empleado', () => {
+    const pipe = new SearchFilterPipe();
+    const name = 'Alberto'
+
+    expect(pipe.transform(employeesListDummyData, name).length > 0).toBeTrue();
+  });
+
+  it('Busqueda por email de empleado', () => {
+    const pipe = new SearchFilterPipe();
+    const email = 'pedro-c@a.com'
+
+    expect(pipe.transform(employeesListDummyData, email)[0]).toEqual(employeesListDummyData[1]);
   });
 });
