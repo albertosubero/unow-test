f2935a0c693cd635ec5155faa8326a3d10717fb6
avsg1991@gmail.com <avsg1991@gmail.com>
2024-04-17 20:34:26 -0500

    UNOW-20: add LoginComponent form feedback errors

--- src/app/core/modules/auth/login/login.component.ts
+++ src/app/core/modules/auth/login/login.component.ts
@@ -3,30 +3,43 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 import { Router } from '@angular/router';
 import { employeesListDummyData } from '../../../../shared/dummyData/employees';
 import { IEmployees } from '../../../../shared/interfaces/employees.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email])
-  });
+  })
   employee!: IEmployees
+  loginFormError: boolean = false
+  loginFormSubmited: boolean = false
+
   constructor(private router: Router) {}
 
   login() {
+    this.loginFormError = false
+    this.loginFormSubmited = false
+    // check if form is valid
     if (this.loginForm.valid) {
+      // check if email is registered
       const employee = this.emailValidation(this.loginForm.value.email!)
       this.employee = employee[0]
+      // if email is registered go to employees view
       if (employee.length) {
         this.saveLoggedUserData(this.employee)
         this.router.navigateByUrl('/employees');
+      } else {
+        this.loginFormError = true
       }
     }
+
+    this.loginFormSubmited = true
   }
   // Check if email is register
   emailValidation(email: string) {
